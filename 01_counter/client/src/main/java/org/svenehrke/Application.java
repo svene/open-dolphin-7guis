1de48aa422aeedd663c0078087a3c9500a30048c
Sven Ehrke <sven.ehrke@canoo.com>
2015-02-18 16:40:10 +0100

    running version of 'counter'

---
+++ 01_counter/client/src/main/java/org/svenehrke/Application.java
@@ -0,0 +1,80 @@
+package org.svenehrke;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import org.opendolphin.binding.JFXBinder;
+import org.opendolphin.core.PresentationModel;
+import org.opendolphin.core.client.ClientDolphin;
+import org.opendolphin.core.client.ClientPresentationModel;
+import org.opendolphin.core.client.comm.OnFinishedHandlerAdapter;
+
+import java.util.List;
+
+import static org.svenehrke.ApplicationConstants.*;
+
+public class Application extends javafx.application.Application {
+    static ClientDolphin clientDolphin;
+
+    private Button button;
+    private Label counterLabel;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.setTitle("7 GUIs: Counter");
+
+        Pane root = setupStage();
+
+        addClientSideAction();
+        initializePMs();
+
+        Scene scene = new Scene(root, 300, 300);
+        scene.setFill(Color.GREEN);
+        stage.setScene(scene);
+        stage.setTitle(getClass().getName());
+        stage.show();
+    }
+
+    private Pane setupStage() {
+        Pane pane = new Pane();
+        HBox hBox = new HBox(10);
+        hBox.setPadding(new Insets(10));
+        hBox.setSpacing(10);
+        button = new Button();
+        counterLabel = new Label("");
+        counterLabel.setFont(Font.font("Verdana", 20));
+
+        pane.getChildren().addAll(hBox);
+		hBox.getChildren().addAll(counterLabel);
+		hBox.getChildren().addAll(button);
+        button.setText("Count");
+        return pane;
+    }
+
+    private void initializePMs() {
+		clientDolphin.send(COMMAND_INIT, new OnFinishedHandlerAdapter() {
+    		@Override
+    		public void onFinished(List<ClientPresentationModel> presentationModels) {
+    			setupBinding();
+    		}
+    	});
+    }
+
+    private void setupBinding() {
+
+        PresentationModel pm = clientDolphin.getAt(PM_APP);
+        JFXBinder.bind(ATT_COUNTER).of(pm).to("text").of(counterLabel);
+    }
+
+    private void addClientSideAction() {
+        button.setOnAction(actionEvent -> clientDolphin.send(COMMAND_INC_COUNTER));
+    }
+}
---
+++ 01_counter/shared/src/main/java/org/svenehrke/ApplicationConstants.java
@@ -0,0 +1,22 @@
+package org.svenehrke;
+
+/**
+ * Place for shared information among client and server. Typically identifiers for models, attributes and actions.
+ */
+public class ApplicationConstants {
+
+    public static final String PM_APP = unique("APP");
+	public static final String ATT_COUNTER = "ATT_COUNTER";
+
+	public static final String COMMAND_INIT = unique("CMD_INIT");
+	public static final String COMMAND_INC_COUNTER = unique("COMMAND_INC_COUNTER");
+
+
+    /**
+     * Unify the identifier with the class name prefix.
+     */
+    private static String unique(String key) {
+        return ApplicationConstants.class.getName() + "." + key;
+    }
+
+}

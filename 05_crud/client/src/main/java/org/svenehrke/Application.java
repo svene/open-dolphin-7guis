9a10538ca68976555475d0d3468e1d0f55810852
Sven Ehrke <sven.ehrke@canoo.com>
2015-04-25 11:39:40 +0200

    using fxml and 'FXMLLoader2' from 'javafx_extensions' project

--- 05_crud/client/src/main/java/org/svenehrke/Application.java
+++ 05_crud/client/src/main/java/org/svenehrke/Application.java
@@ -1,26 +1,15 @@
 package org.svenehrke;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import org.opendolphin.binding.JFXBinder;
-import org.opendolphin.core.PresentationModel;
 import org.opendolphin.core.client.ClientDolphin;
-import org.opendolphin.core.client.ClientPresentationModel;
-import org.opendolphin.core.client.comm.OnFinishedHandlerAdapter;
-
-import static org.svenehrke.ApplicationConstants.*;
-
-import java.util.List;
+import org.svenehrke.javafxextensions.fxml.FXMLLoader2;
+import org.svenehrke.javafxextensions.fxml.ViewAndRoot;
 
 public class Application extends javafx.application.Application {
     static ClientDolphin clientDolphin;
@@ -35,60 +24,22 @@ public class Application extends javafx.application.Application {
     }
 
     private Pane rootView() {
-        Pane pane = new Pane();
-        VBox vBox = new VBox(10);
-        vBox.setPadding(new Insets(10));
-        vBox.setSpacing(10);
-        button = new Button();
-        nameTextField = new TextField();
-        greetingLabel = new Label("");
-        greetingLabel.setFont(Font.font ("Verdana", 20));
-
-        pane.getChildren().addAll(vBox);
-        vBox.getChildren().addAll(nameTextField);
-        vBox.getChildren().addAll(button);
-        vBox.getChildren().addAll(greetingLabel);
-        button.setText("Greet");
-        return pane;
+        final ViewAndRoot<MainView, BorderPane> cr = FXMLLoader2.loadFXML("/Main.fxml");
+        MainView view = cr.getView();
+        Pane rootLayout = cr.getRoot();
+        return rootLayout;
     }
 
     private void bootstrap(final Stage stage) {
 
 		Pane root = rootView();
-		addClientSideAction();
-
-		clientDolphin.send(COMMAND_INIT, new OnFinishedHandlerAdapter() {
-    		@Override
-    		public void onFinished(List<ClientPresentationModel> presentationModels) {
-    			setupBinding();
-				stage.show();
-    		}
-    	});
 
 		Scene scene = new Scene(root, 300, 300);
         scene.getStylesheets().add(getClass().getResource("/app.css").toExternalForm());
 		stage.setScene(scene);
 		stage.setTitle("7 GUIs: CRUD");
+        stage.show();
 	}
 
-    private void setupBinding() {
 
-        PresentationModel pm = clientDolphin.getAt(PM_APP);
-
-        JFXBinder.bind(ATT_NAME).of(pm).to("text").of(nameTextField);
-        JFXBinder.bind("text").of(nameTextField).to(ATT_NAME).of(pm);
-
-        JFXBinder.bind(ATT_GREETING).of(pm).to("text").of(greetingLabel);
-
-        clientDolphin.getAt(PM_APP).getAt(ATT_NAME).setValue("Duke");
-    }
-
-    private void addClientSideAction() {
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                clientDolphin.send(COMMAND_GREET);
-            }
-        });
-    }
 }
---
+++ 05_crud/client/src/main/java/org/svenehrke/MainView.java
@@ -0,0 +1,4 @@
+package org.svenehrke;
+
+public class MainView {
+}
